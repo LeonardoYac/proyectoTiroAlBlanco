f08ea32987c01d07d086ca565d62cef481c8970c
LeonardoYac <alejandroyac333@gmail.com>
2021-04-27 16:08:13 -0600

    reinicio y bloqueo

--- main.c
+++ main.c
@@ -29,8 +29,12 @@ void GPIO(void){ // configuracion inicial
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);    // DISPLAY
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);    // LDR
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);    // CONMUTADOR DISPLAY
+    // para desblockear el PF0 SW2
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+    GPIO_PORTF_CR_R = 0x0f;
 
     // definir entradas o salidas
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0); // BTN
     GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6  | GPIO_PIN_7 ); // ldr
 
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4); // leds
@@ -38,7 +42,8 @@ void GPIO(void){ // configuracion inicial
     GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_0 | GPIO_PIN_1);    // conmutador
 
     // configuraciones pullUp o pullDown [ruidos]
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6  | GPIO_PIN_7, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU); // puetos, pines a utilizar, corrientes, PULLUP O PULLDOWN WPU|WPD
+    GPIOPadConfigSet(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6  | GPIO_PIN_7, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU); // puetos, pines a utilizar, corrientes, PULLUP O PULLDOWN WPU|WPD
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU); // RESET
 }
 
 void Delay2(void){                  // tiempo para la multiplexacion.
@@ -91,14 +96,16 @@ void conteo(void){      // para mostrar en el display contador.
 }
 
 void moduloUno(void){           //** modulo UNO-LDR.
+    int uno = 0;
     while(pos1 <  miTiempo){                                                                    // ciclo de aprox 1s. para detectar la senyal.
         moduloLDR = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);  // lectura del modulo fisio ldr.
 
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4, 2);    // muestra el led indicador del blanco.
         pos1++;                                                                                 // pos1 incrementa en uno.
 
-        if(moduloLDR == 224 && before == 0){                                                    // si es el primer modulo y begore = 0 ingresa.
+        if(moduloLDR == 224 && before == 0 && uno == 0){                                                    // si es el primer modulo y begore = 0 ingresa.
            conteo();                                                                            // evalua el valor para enviarlo al display.
+           uno++;
         }
         if(moduloLDR == 240){                                                                   // si todos los modulos dan 1.
            before = 0;                                                                          // variable before regresa a 0 para ya no aceptar valores.
@@ -111,14 +118,16 @@ void moduloUno(void){           //** modulo UNO-LDR.
 }
 
 void moduloDos(void){           //** modulo DOS-LDR.
+    int uno = 0;
     while(pos2 < miTiempo){
         moduloLDR = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
 
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4, 4);
         pos2++;
 
-        if(moduloLDR == 208 && before == 0){ // MOD 2
+        if(moduloLDR == 208 && before == 0 && uno == 0){ // MOD 2
            conteo();
+           uno++;
         }
         if(moduloLDR == 240){
            before = 0;
@@ -131,14 +140,16 @@ void moduloDos(void){           //** modulo DOS-LDR.
 }
 
 void moduloTres(void){          //** modulo TRES-LDR.
+    int uno = 0;
     while(pos3 < miTiempo){
         moduloLDR = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
 
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4, 8);
         pos3++;
 
-        if(moduloLDR == 176 && before == 0){ // MOD 3
+        if(moduloLDR == 176 && before == 0 && uno == 0){ // MOD 3
            conteo();
+           uno++;
         }
         if(moduloLDR == 240){
            before = 0;
@@ -151,14 +162,16 @@ void moduloTres(void){          //** modulo TRES-LDR.
 }
 
 void moduloCuatro(void){            //** modulo CUATRO-LDR.
+    int uno = 0;
     while(pos4 < miTiempo){
         moduloLDR = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
 
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4, 16);
         pos4++;
 
-        if(moduloLDR == 112 && before == 0){ // MOD 4
+        if(moduloLDR == 112 && before == 0 && uno == 0){ // MOD 4
            conteo();
+           uno++;
         }
         if(moduloLDR == 240){
            before = 0;
@@ -204,6 +217,7 @@ int main (void){
     // *** configuracion del reloj del dispositivo ***
     SysCtlClockSet(SYSCTL_OSC_MAIN| SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_5); // configurando el proyecto a 40 MHz
     GPIO();                                                     // configuracion principal puertos.
+    bool ciclo = true;
 
     conteoInicio();                                             // se llama al metodo para la cuenta regresiva. 3>>2>>1.
     miTiempo = 1000;                                            // aprox 1 segundo.
@@ -213,23 +227,40 @@ int main (void){
     pos3 = 0;
     pos4 = 0;
 
-    // *** secuencia uno
-    secuenciaUno();                                             // se llama al metodo secuenciUno.
-    int espera;                                                 // al finalizar la secuencia y se espera 3-4 seg aprox.
-    for(espera = 0; espera <= 4000; espera++){                  // se muestra el valor acumulado en el display.
-        Display();
+
+    while(true){
+        // *** secuencia uno
+        secuenciaUno();                                             // se llama al metodo secuenciUno.
+        int espera;                                                 // al finalizar la secuencia y se espera 3-4 seg aprox.
+        for(espera = 0; espera <= 4000; espera++){                  // se muestra el valor acumulado en el display.
+            Display();
+        }
+
+        // *** secuencia dos
+        secuenciaDos();
+        for(espera = 0; espera <= 4000; espera++){
+            Display();
+        }
+        // *** secuencia tres.
+        secuenciaTres();
+        ciclo = true;
+        while(ciclo){                                                   // al finalizar la secuencia el ciclo muestra el display con el valor final.
+            Display();
+            int btn = 0;
+            btn = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+            if(btn != 1){
+                conteoInicio();
+                miTiempo = 1000;
+                CONT = 0;
+                pos1 = 0;
+                pos2 = 0;
+                pos3 = 0;
+                pos4 = 0;
+                ciclo = false;
+            }
+        }
     }
 
-    // *** secuencia dos
-    secuenciaDos();
-    for(espera = 0; espera <= 4000; espera++){
-        Display();
-    }
 
-    // *** secuencia tres.
-    secuenciaTres();
-
-    while(1){                                                   // al finalizar la secuencia el ciclo muestra el display con el valor final.
-        Display();
-    }
+// fallo btn-inicio solo al final -- implementar reset.  |interrupciones|
 }
